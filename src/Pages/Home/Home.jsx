c98ffeaf024fc8c71f50378f0cd5cbac76f7e116
aridguy <rotimiariyo@gmail.com>
2022-12-14 22:55:07 +0100

    created the profile picture toggle function on home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from '../../Assets/Logo.png'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css'
@@ -9,8 +9,14 @@ import Personnel from '../../Assets/Iconics/personnel.svg'
 import Analytics from '../../Assets/Iconics/analytics.svg'
 import Tickets from '../../Assets/Iconics/tickets.svg'
 
+
 const Home = () => {
     // <a style={Styles2.atyles} className="nav-link" href="localhost:3000">Contact Support</a>
+
+    const openModal = () =>{
+        setOpen(!open)
+    }
+    const [open, setOpen] = useState(false)
 
     return (
 
@@ -25,16 +31,18 @@ const Home = () => {
                     <div className="collapse navbar-collapse justify-content-end" id="collapsibleNavbar">
                         <ul className="navbar-nav ">
                             <li className="nav-item">
-                                <div className="profile text-center justify-items-center"><b className="p-txt">AM</b></div>
+                                <div onClick={openModal} className="profile text-center justify-items-center"><b className="p-txt">AM</b></div>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
-            <div className="modals">
+
+            { open?
+                <div className="modals">
                 <div className="container">
                     <div className="row">
-                    <div className="col-md-4"></div>
+                        <div className="col-md-4"></div>
                         <div className="col-md-4">
                             <div className="modalProfiles">
                                 <b className="p-txt">AM</b>
@@ -42,8 +50,29 @@ const Home = () => {
                         </div>
                         <div className="col-md-4"></div>
                     </div>
+                    <div className="row mt-3">
+                        <div className="col-md-12 text-center">
+                            <p className="extraTxt2">AutoMedics Africa Limited</p>
+                            <smalls>2 agents license</smalls>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <button className="botin2">Manage your Recallo Account</button>
+                        </div>
+                    </div>
+                    <div className="row mt-1">
+                        <div className="col-md-12 text-center">
+                            <hr />
+                            <smalls>Log out</smalls>
+                        </div>
+                    </div>
                 </div>
             </div>
+            :
+            null
+            }
+
             <div className="universalWrap">
                 <div className="sidebar">
                     <div className="container">
